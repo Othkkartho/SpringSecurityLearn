4339fbf2c43a9454e3d02b83797cf617e7e07814
qhtkdrma <kimwonkyu15@gmail.com>
2022-09-30 19:49:43 +0900

    기본 파일

---
+++ src/main/java/com/example/springsecuritylearn/SpringSecurityLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritylearn/SpringSecurityLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
