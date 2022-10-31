ae2cc847cd3f07002b26e7ebeac038180d12e128
qhtkdrma <kimwonkyu15@gmail.com>
2022-10-31 19:11:44 +0900

    authenticationManager must be specified 오류 해결

--- src/main/java/com/example/springsecuritylearn/security/filter/AjaxLoginProcessingFilter.java
+++ src/main/java/com/example/springsecuritylearn/security/filter/AjaxLoginProcessingFilter.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
 public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingFilter {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
